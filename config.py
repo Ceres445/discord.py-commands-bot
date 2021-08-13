28e28fe101123e4f8547382ba35800fc06c4ea5c
Ceres <Ceres445@users.noreply.github.com>
2021-08-13 12:05:14 +0530

    Removed postgres connection URI

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ import datetime
 def load_vars():
     """Load heroku/ qovery/ local environment variables"""
     try:
-        print(os.environ.get('DATABASE_URL'), os.environ.get('TOKEN'), os.environ.get('TZ'))
-        postgres = os.environ['DATABASE_URL']
+        print(os.environ.get('TOKEN'), os.environ.get('TZ'))
+        # postgres = os.environ['DATABASE_URL']
         token = os.environ['TOKEN']
         print("time is ", datetime.datetime.now())
         print('loaded heroku env variables')
     except KeyError:
         try:
-            postgres = os.environ['QOVERY_DATABASE_BOT_CONNECTION_URI']
+            # postgres = os.environ['QOVERY_DATABASE_BOT_CONNECTION_URI']
             token = os.environ['token']
             print('loaded qovery variables')
         except KeyError:
             load_dotenv()
             print('loaded local dotenv file')
             token = os.environ['TOKEN']
-    return postgres, token
+    return token
